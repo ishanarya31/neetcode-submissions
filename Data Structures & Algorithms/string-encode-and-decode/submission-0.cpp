35e33fa051a1a1e305b1901de9845777dea0b5f7
Ishan <146348455+ishanarya31@users.noreply.github.com>
2026-06-30 23:39:14 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> res;
+    string encode(vector<string>& strs) {
+        int n = strs.size();
+        res = strs;
+        return "";
+    }
+
+    vector<string> decode(string s) {
+        return res;
+    }
+};
